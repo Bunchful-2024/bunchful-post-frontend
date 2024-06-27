b194f0b6e232c54da5820f02bbbe47de4313700f
Chi Chen <stephchen55@gmail.com>
2024-06-27 11:57:03 +0800

    create section 1 with multi selection

--- Post.py
+++ Post.py
@@ -4,6 +4,20 @@ import streamlit as st
 st.title("Bunchful Post")
 st.caption("Welcome to Bunchful Post! Manage your content here.")
 
+# Section 1: Content Curation
+st.subheader("Step1: Content Curation")
+st.write("Modify your content with the help of AI.")
+# Defining Multi_Select with Pre-Selection
+topics = st.multiselect(
+       "Select your topic",
+       ['1: No Poverty', '2: Zero Hunger', '3: Good Health and Well-Being', '4: Quality Education',
+        '5: Gender Equality', '6: Clean Water and Sanitation', '7: Affordable and Clean Energy',
+        '8: Decent Work and Economic Growth', '9: Industry, Innovation, and Infrastructure',
+        '10: Reduced Inequalities', '11: Sustainable Cities and Communities', '12: Responsible Consumption and Production',
+        '13: Climate Action', '14: Life Below Water', '15: Life on Land', '16: Peace, Justice, and Strong Institutions','17: Partnerships for the Goals'],
+       ['1: No Poverty','17: Partnerships for the Goals'])
+
+
 
 # Sidebar
 st.sidebar.title("Help")
