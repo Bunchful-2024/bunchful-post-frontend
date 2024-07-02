c5f6f24c7c0244003cdf19b52f0ee6355e172ed0
Chi Chen <stephchen55@gmail.com>
2024-07-03 00:16:24 +0800

    set up blog generate function

--- Post.py
+++ Post.py
@@ -1,8 +1,11 @@
+import os
 import streamlit as st
 import services.prompts as prompts
 from openai import OpenAI
 
-clien = OpenAI()
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 # Title
 st.title("🩵 Bunchful Post")
@@ -11,6 +14,7 @@ st.caption("Welcome to Bunchful Post! Manage your content here.")
 # Section 1: Content Curation
 st.subheader("Step1: Content Curation")
 st.write("Customize your content with the help of AI.")
+
 # SDGs Topic Select 
 topics = st.multiselect(
        "Select your topic:",
@@ -20,29 +24,34 @@ topics = st.multiselect(
         '10: Reduced Inequalities', '11: Sustainable Cities and Communities', '12: Responsible Consumption and Production',
         '13: Climate Action', '14: Life Below Water', '15: Life on Land', '16: Peace, Justice, and Strong Institutions','17: Partnerships for the Goals'],
        ['1: No Poverty','17: Partnerships for the Goals'])
+
 # Content Type
 content_type = st.selectbox(
         'Select your content type: ',
         ('Blog', 'Newsletter', 'Social Meida Post'),
         index=0)
+
 # Initial Draft
 input_draft = st.text_area("Enter your Draft")
+
 # Printing entered text
 st.write("""You entered:  \n""",input_draft)
+
 # Generate Button
 button_generate = st.button("Generate")
+
 # Mimic Generate Button Logic
 if button_generate:
     if content_type == 'Blog':
-
-        st.write(
-            '''
-            Eradicating extreme poverty for all people everywhere by 2030 is a pivotal goal of the 2030 Agenda for Sustainable Development. 
-            Extreme poverty, defined as surviving on less than $2.15 per person per day at 2017 purchasing power parity, has witnessed remarkable declines over recent decades. 
-            
-            However, the emergence of COVID-19 marked a turning point, reversing these gains as the number of individuals living in extreme poverty increased for the first time in a generation by almost 90 million over previous predictions.
-            '''
+        blog_prompt = prompts.generate_blog_prompt(topics, input_draft)
+        generated_response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": prompts.general_prompt()},
+                {"role": "user", "content": prompts.generate_blog_prompt(topics, input_draft)},
+            ]
         )
+        st.write(generated_response.choices[0].message.content)
 
 
 # Sidebar for guidance
--- services/prompts.py
+++ services/prompts.py
@@ -1,12 +1,16 @@
 #predefined prompts for openai api to use from
+
+def general_prompt():
+    return """
+    You are a professional blog writer about UN SDGs.  
+    You assist users in composing content for their platforms.
+    """
+   
 def generate_blog_prompt(topics: list, draft: str) -> str:
     topics_str = ', '.join(topics)
     return f"""
-    You are a professional blog writer about UN SDGs.  
-    You assist users in composing content for their platforms.
-
     These are the provided UN SDGs topics: ```{topics_str}```.
-    This is the initial draft provided by user: ```{draft}```.
+    This is the initial draft: ```{draft}```.
     Generate a blog based on the provided information, 
     consider the target audience for blog content type,
     and make sure the word count is with 1000-1500 words.
