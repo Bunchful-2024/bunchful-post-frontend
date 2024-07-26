2dc36c4bb5f879e4fad719239f4eeafbbae3d79f
Chi Chen <stephchen55@gmail.com>
2024-07-26 17:18:30 -0400

    (fb) update requirements.txt and add publish button logic

--- Post.py
+++ Post.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 import requests
+import json
 import services.prompts as prompts
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -9,6 +10,10 @@ load_dotenv()
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
+
+# Access environment variables
+fb_page_id = os.environ.get('FB_PAGE_ID')
+fb_access_token = os.environ.get('FB_PAGE_ACCESS_TOKEN')
 
 # Title
 st.title("🩵 Bunchful Post")
@@ -53,10 +58,10 @@ button_generate = st.button("Generate")
 
 # Mimic Generate Button Logic/Put Gemini logic here
 generated_text = '''
-    Eradicating extreme poverty for all people everywhere by 2030 is a pivotal goal of the 2030 Agenda for Sustainable Development. 
-    Extreme poverty, defined as surviving on less than $2.15 per person per day at 2017 purchasing power parity, has witnessed remarkable declines over recent decades. 
+Eradicating extreme poverty for all people everywhere by 2030 is a pivotal goal of the 2030 Agenda for Sustainable Development. 
+Extreme poverty, defined as surviving on less than $2.15 per person per day at 2017 purchasing power parity, has witnessed remarkable declines over recent decades. 
         
-    However, the emergence of COVID-19 marked a turning point, reversing these gains as the number of individuals living in extreme poverty increased for the first time in a generation by almost 90 million over previous predictions.
+However, the emergence of COVID-19 marked a turning point, reversing these gains as the number of individuals living in extreme poverty increased for the first time in a generation by almost 90 million over previous predictions.
 '''
 if button_generate:
     if platforms == 'Facebook':
@@ -64,6 +69,30 @@ if button_generate:
         st.write(generated_text)
 
         publish_button = st.button("Publish")
+        #publish content to FB page
+        if publish_button:
+            # Facebook API endpoint for posting to a page
+            fb_api_url = f'https://graph.facebook.com/v20.0/{fb_page_id}/feed'
+
+            payload = {
+                'message': generated_text,
+                'access_token': fb_access_token
+            }
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            # Debugging: Use st.write to display the payload and URL
+            st.write("Facebook API URL:", fb_api_url)
+            st.write("Payload:", payload)
+            st.write("Headers:", headers)
+
+            response = requests.post(fb_api_url, headers=headers, data=json.dumps(payload))
+
+            if response.status_code == 200:
+                st.success("Post published successfully on Facebook!")
+            else:
+                st.error(f"Failed to publish post: {response.text}")
 
 # original OpenAI API logic
 # if button_generate:
