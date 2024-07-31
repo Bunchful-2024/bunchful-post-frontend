7b5903599037bb245f6340dbcd8c0032e59efecd
Chi Chen <stephchen55@gmail.com>
2024-07-31 11:28:59 -0400

    manually merge codes from SCRUM-4 UI

--- Post.py
+++ Post.py
@@ -1,14 +1,15 @@
-import os
-import streamlit as st
-import requests
 import json
+import os
+import requests
+import streamlit as st
+from services.prompts import general_prompt  
+from services.functions import extract_generated_content
 import google.generativeai as genai
-from services.prompts import general_prompt 
 from dotenv import load_dotenv
 
+# Load and set up environment variables
 load_dotenv()
 genai.configure(api_key=os.getenv("API_KEY"))
-
 model = genai.GenerativeModel('gemini-1.5-pro')
 
 # Access environment variables
@@ -16,39 +17,66 @@ fb_page_id = os.environ.get('FB_PAGE_ID')
 fb_access_token = os.environ.get('FB_PAGE_ACCESS_TOKEN')
 fb_access_token = str(fb_access_token)
 
-# Initialize session state for generated text
-if 'generated_text' not in st.session_state:
-    st.session_state.generated_text = ''
+# Set up initial session state
+if 'content_type' not in st.session_state:
+    st.session_state.content_type = ""
+if 'platforms' not in st.session_state:
+    st.session_state.platforms = []
+if 'generate_all' not in st.session_state:
+    st.session_state.generate_all = False
+if 'keyword' not in st.session_state:
+    st.session_state.keyword = ""
+if 'topic' not in st.session_state:
+    st.session_state.topic = ""
 
 # Title
-st.title("🩵 Bunchful Post")
+st.title("🙌 Bunchful Post")
 st.caption("Welcome to Bunchful Post! Manage your content here.")
 
-# Section 1: Content Curation
-st.subheader("Step1: Content Curation")
-st.write("Customize your content with the help of AI.")
+# Step 1: Enter Topic
+st.markdown("#### Step 1: Enter Topic")
+st.session_state.topic = st.text_input("Enter your topic here:")
 
-# Text area for entering the topic/keyword
-topic = st.text_area("Enter your Topic/Keyword")
+# Step 2: Enter Keywords
+st.markdown("#### Step 2: Enter Keywords")
+st.session_state.keyword = st.text_input("Enter your keywords here:")
 
-st.divider()
+# Step 3: Select Content Type
+st.markdown("#### Step 3: Select Content Type")
+content_choices = ["Social Media Post", "Video Scripts", "Articles", "Blogs", "Ads", "Case Study", "Press Release", 
+                "Emails - Promotional", "Emails - Cold", "Emails - Outbounds", "Emails - Warm", "Newsletters", "Welcome", "SMS Messages", "Job Posts"]
+st.session_state.content_type = st.selectbox("Select your content type:", content_choices, index=0)
 
-# Content Type
-st.write("Select your Content Type:")
+# Step 4: Select Platform
+# dictionary to map content types to platforms
+content_to_platform = {
+    "Social Media Post": ["LinkedIn", "Facebook", "Instagram", "X (Twitter)", "Pinterest", "Youtube", "TikTok", "Threads"],
+    "Video Scripts": ["Website", "Facebook", "Instagram", "YouTube", "TikTok", "Threads"],
+    "Articles": ["Website", "Reddit", "Medium", "Hub Pages", "Vocal Media", "NewsBreak", "Steemit", "Ghost", "Write.as"],
+    "Blogs": ["Website", "Tumblr"],
+    "Ads": ["Website", "Amazon", "Bing", "Google", "LinkedIn", "Facebook", "Instagram", "X (Twitter)", "Pinterest", "YouTube", "TikTok", "Threads"],
+    "Case Study": [],
+    "Press Release": [],
+    "Emails - Promotional": [],
+    "Emails - Cold": [],
+    "Emails - Outbounds": [],
+    "Emails - Warm": [],
+    "Newsletters": [],
+    "Welcome": [],
+    "SMS Messages": ["Idealist"],
+    "Job Posts": []
+}
 
-# Platfrom select
-platforms = st.multiselect(
-    "Select your platform:",
-    ['LinkedIn', 'Instagram', 'Facebook', 'X (Twitter)', 'Instagram Thread']
-)
-
+st.markdown("#### Step 4: Select Platform")
+st.session_state.generate_all = st.checkbox("Generate for all platforms", value=False)
+st.session_state.platforms = st.multiselect("Select your platform:", content_to_platform[st.session_state.content_type], disabled=st.session_state.generate_all)
 
 # Range for character limits
 min_char_limit = st.slider("Minimum Character Limit", min_value=100, max_value=2000, value=280)
 max_char_limit = st.slider("Maximum Character Limit", min_value=100, max_value=2000, value=1000)
 
 # Generate button
-button_generate = st.button("Generate")
+generate_button = st.button("Generate")
 
 # Platform character limits (for default values if range is not specified)
 platform_character_limits = {
@@ -56,21 +84,22 @@ platform_character_limits = {
     'Facebook': 1500,
     'Instagram': 1300,
     'X (Twitter)': 280,
-    'Instagram Threads': 500
+    'Instagram Threads': 500,
+    'Medium': 2000,
 }
 
 # Mimic Generate Button Logic/Put Gemini logic here
-if button_generate:
+if generate_button:
     try:
         # Process each selected platform
-        for platform in platforms:
+        for platform in st.session_state.platforms:
             # Use platform default limit if it falls within the user-specified range
             character_limit = min(max_char_limit, platform_character_limits.get(platform, 500))
             character_limit = max(min_char_limit, character_limit)
 
             # Generate prompt based on the platform and character limit
             prompt = general_prompt(platform, character_limit)
-            prompt = prompt.format(Topic=topic)
+            prompt = prompt.format(Topic=str(st.session_state.topic))
 
             # Calculate estimated token count
             prompt_tokens = len(prompt.split())
@@ -80,15 +109,16 @@ if button_generate:
             response = model.generate_content(prompt)
 
             # Accessing the content from the response object
-            generated_text = response.text
+            generated_result = response.text
+            generated_text = extract_generated_content(response.text)
+            #print(generated_result) #for testing
             generated_char_count = len(generated_text)
             input_tokens = response.usage_metadata.prompt_token_count
             output_tokens = response.usage_metadata.candidates_token_count
             
-            # Display results and store generated text in session state
-            st.markdown(f"### Generated Content for {platform}:")
-            st.write(generated_text)
-            st.session_state.generated_text = generated_text
+            # Display results
+            st.markdown(f"### Generated Result for {platform}:")
+            st.write(generated_result)
 
             # Display character counts and cost projection
             st.markdown("### Writer AI Cost projection per article")
@@ -99,13 +129,16 @@ if button_generate:
             token_cost = input_tokens * 0.0000075 + output_tokens * 0.0000225
             st.write(f"Estimated cost: ${token_cost:.6f}")
 
+            st.markdown("### Edit Section")
+            st.text_area("Prompt", value=generated_text, height=200)
+
     except AttributeError as e:
         st.error(f"An attribute error occurred: {e}")
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
 if st.session_state.generated_text:
-    if platforms == ['Facebook']:
+    if st.session_state.platforms == ['Facebook']:
         st.subheader("Step 2: Publish to Facebook")
         publish_button = st.button("Publish")
         #publish content to FB page
