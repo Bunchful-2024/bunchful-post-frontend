508237e3450ede5ef11a6f276b4f89c265abc4dd
Chi Chen <stephchen55@gmail.com>
2024-08-08 15:44:31 -0400

    add reminder for text edit section

--- Post.py
+++ Post.py
@@ -208,6 +208,7 @@ if generate_button:
 if st.session_state.generated_text:
 
     st.markdown("### Edit Section")
+    st.write("If you are modifying the image placment, please ensure you copy the whole image info in the format [Image X: Caption].")
     st.session_state.formatted_text = transform_to_markdown(st.session_state.generated_text)
     st.session_state.edited_text = st.text_area("Edit your content:", value=st.session_state.generated_text, height=500)
     print(st.session_state.formatted_text) #for testing
